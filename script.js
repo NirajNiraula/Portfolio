f7433c748a047cf3682b9c1bb331624fcd726ca3
NirajNiraula <niraulaniraj786@gmail.com>
2022-07-11 16:25:13 +0545

    secind

---
+++ script.js
@@ -0,0 +1,12 @@
+let arr=[1,2,3];
+//const double = (a)=>{
+//let doubarr=[];
+//for(let i=0;i<arr.length;i++){
+  //  doubarr.push(a[i]*2);
+// }
+//return doubarr;
+//}
+//console.log('double',double(arr));
+arr.map((index,val) => {
+    console.log(index,val);
+})
